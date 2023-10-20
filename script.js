215e5e9409fb766807925c080f5002504cc04ebd
777dan <danya.nikitin7@gmail.com>
2023-10-20 20:08:49 +0300

    added array of strokes

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const str1 = "Lorem ipsum";
-const str2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nostrum repudiandae, officia ullam fuga dolor sequi placeat id recusandae enim eligendi quasi cupiditate quisquam natus, laborum aperiam, voluptatum cum obcaecati!";
-function preview(str, n) {
-    if (str.length > n) {
-        str = str.substr(0, n).concat("...");
+const strokes = [
+    "Lorem ipsum",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nostrum repudiandae, officia ullam fuga dolor sequi placeat id recusandae enim eligendi quasi cupiditate quisquam natus, laborum aperiam, voluptatum cum obcaecati!"
+];
+function preview(strokes, n) {
+    for (let i = 0; i < strokes.length; i++) {
+        if (strokes[i].length > n) {
+            strokes[i] = strokes[i].substr(0, n);
+            strokes[i] = strokes[i].substr(0, strokes[i].lastIndexOf(" "));
+            strokes[i] = strokes[i].concat("...");
+        }
     }
-    return str;
+    return strokes;
 }
 
-console.log(preview(str2, 11))
+console.log(preview(strokes, 20))
